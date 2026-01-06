c211541f67aa82cea3e922a0f1af71426a16e40c
PieroSanchez <pierosancas@gmail.com>
2026-01-06 11:48:36 -0500

    vista ordenes tf

--- src/app/features/orden-prod-tf/orden-prod-tf.component.ts
+++ src/app/features/orden-prod-tf/orden-prod-tf.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { OrdenTrabajoTF } from '../../core/models/plan-prod-tf';
 import { PlanProdTfService } from '../../core/services/plan-prod-tf.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-orden-prod-tf',
   standalone: true,
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, DragDropModule, FormsModule],
   templateUrl: './orden-prod-tf.component.html',
   styleUrl: './orden-prod-tf.component.css'
 })
@@ -16,6 +17,20 @@ export class OrdenProdTfComponent implements OnInit{
   listaOTs: OrdenTrabajoTF[] = [];
   otSeleccionada: OrdenTrabajoTF | null = null;
 
+  registroActual = {
+    codigoBobina: '',
+    loteBobina: '',
+    nombreBobina: '',
+    velocidad: 0,
+    horaInicio: '',
+    horaFin: '',
+    cajas: 0,
+    rechazoKg: 0
+  };
+
+  listaDetalles: any[] = [];
+  observacionesGenerales: string = '';
+
   constructor(private otService: PlanProdTfService) { }
 
   ngOnInit(): void {
@@ -23,6 +38,7 @@ export class OrdenProdTfComponent implements OnInit{
   }
 
   cargarOrdenesPendientes() {
+
     this.otService.listarot().subscribe({
       next: (data) => {
         this.listaOTs = data; 
@@ -47,5 +63,44 @@ export class OrdenProdTfComponent implements OnInit{
 
   seleccionarOT(ot: OrdenTrabajoTF) {
     this.otSeleccionada = ot;
+    this.listaDetalles = []; 
+    this.limpiarFormulario();
+  }
+
+  buscarBobina() {
+    if (!this.registroActual.codigoBobina) return;
+    console.log("Buscando bobina:", this.registroActual.codigoBobina);
+    
+    this.registroActual.loteBobina = 'LOTE-SIM-' + Math.floor(Math.random() * 1000);
+    this.registroActual.nombreBobina = 'Bobina PET Transparente (Simulada)';
+    
+
+    const now = new Date();
+    this.registroActual.horaInicio = now.toTimeString().substring(0, 5); 
+  }
+
+  agregarRegistro() {
+    if (!this.registroActual.codigoBobina) return;
+    this.listaDetalles.push({ ...this.registroActual });
+    this.limpiarFormulario();
+  }
+
+  limpiarFormulario() {
+    this.registroActual = {
+      codigoBobina: '',
+      loteBobina: '',
+      nombreBobina: '',
+      velocidad: 0,
+      horaInicio: '',
+      horaFin: '',
+      cajas: 0,
+      rechazoKg: 0
+    };
+  }
+
+  guardarAvance() {
+    console.log('Guardando avance de OT:', this.otSeleccionada?.codigo);
+    console.log('Detalles:', this.listaDetalles);
+    console.log('Obs:', this.observacionesGenerales);
   }
 }
